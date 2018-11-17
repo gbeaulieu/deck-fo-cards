c24d1dd7733e756f3a192349a842b40aa8206343
Gabriel Beaulieu <gabrielbeaulieu117@gmail.com>
2018-11-17 15:10:58 -0500

    Move files to right folder

--- src/app/board/board-routing.module.ts
+++ src/app/board/board-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BoardComponent } from './components/board/board.component';
+import { BoardComponent } from './board.component';
 
 const routes: Routes = [
     {
--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BoardComponent } from './components/board/board.component';
-import { BoardRoutingModule } from './board-routine.module';
+import { BoardComponent } from './board.component';
+import { BoardRoutingModule } from './board-routing.module';
 
 @NgModule({
   declarations: [
