70349329495ba018bb4edeb01a4c02ed44e50fac
Gabriel Beaulieu <gabrielbeaulieu117@gmail.com>
2018-11-17 17:23:50 -0500

    Use boardservice in BoardComponent to generate array of cards

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,38 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Card } from './models/card';
-import { CardKind } from './models/card-kind';
+import { BoardService } from './services/board.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent {
-  public cards: Card[] = [
-    {
-      id: 1,
-      kind: CardKind.Heart,
-      faceValue: 'Ace'
-    },
-    {
-      id: 2,
-      kind: CardKind.Heart,
-      faceValue: '2'
-    },
-    {
-      id: 3,
-      kind: CardKind.Heart,
-      faceValue: '3'
-    },
-    {
-      id: 4,
-      kind: CardKind.Heart,
-      faceValue: '4'
-    },
-    {
-      id: 5,
-      kind: CardKind.Heart,
-      faceValue: '5'
-    }
-  ];
+export class BoardComponent implements OnInit {
+  public cards: Card[];
+
+  constructor(private _boardService: BoardService) {}
+
+  public ngOnInit() {
+    this.cards = this._boardService.generateDeck();
+  }
 }
