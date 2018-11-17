a74e44736c248c7c3f35e88debaea45d1cd90bb5
Gabriel Beaulieu <gabrielbeaulieu117@gmail.com>
2018-11-17 15:39:12 -0500

    Create CardListComponent

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board.component';
 import { BoardRoutingModule } from './board-routing.module';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    BoardComponent
+    BoardComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule,
