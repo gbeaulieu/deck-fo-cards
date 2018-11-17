17fa003e29b45e3c5661458121d337e954c95851
Gabriel Beaulieu <gabrielbeaulieu117@gmail.com>
2018-11-17 17:23:24 -0500

    Implement a boardservice to generate and shuffle array of cards

---
+++ src/app/board/services/board.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BoardService } from './board.service';
+import { Card } from '../models/card';
+
+describe('BoardService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: BoardService = TestBed.get(BoardService);
+    expect(service).toBeTruthy();
+  });
+
+  describe('generateDeck', () => {
+    it('should return an array with a length of 52', () => {
+      const service: BoardService = TestBed.get(BoardService);
+      const deck = service.generateDeck();
+      expect(deck.length).toBe(52);
+    });
+  });
+
+  describe('suffleDeck', () => {
+    let service: BoardService;
+    let deck: Card[];
+
+    beforeEach(() => {
+      service = TestBed.get(BoardService);
+      deck = service.generateDeck();
+      expect(deck.length).toBe(52);
+    });
+
+    it('should not alter array length', () => {
+      deck = service.generateDeck();
+      service.shuffleDeck(deck);
+
+      expect(deck.length).toBe(52);
+    });
+
+    it('should shuffle the first card', () => {
+      deck = service.generateDeck();
+      const originalDeck = Object.assign({}, deck);
+
+      service.shuffleDeck(deck);
+
+      expect(deck[0]).not.toBe(originalDeck[0]);
+    });
+  });
+});
---
+++ src/app/board/services/board.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Card } from '../models/card';
+import { CardKind } from '../models/card-kind';
+import { FaceValue } from '../models/face-value';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+
+  private readonly faceValues: FaceValue[] = [
+    FaceValue.Ace,
+    FaceValue.Two,
+    FaceValue.Three,
+    FaceValue.Four,
+    FaceValue.Five,
+    FaceValue.Six,
+    FaceValue.Seven,
+    FaceValue.Eight,
+    FaceValue.Nine,
+    FaceValue.Ten,
+    FaceValue.Jack,
+    FaceValue.Queen,
+    FaceValue.King
+  ];
+
+  private readonly cardKinds: CardKind[] = [
+    CardKind.Heart,
+    CardKind.Spade,
+    CardKind.Club,
+    CardKind.Diamond
+];
+
+  constructor() { }
+
+  /**
+  * Generate an array of 52 cards containing 13 cards of each kind
+  * @returns returns an array of Card
+  */
+  public generateDeck(): Card[] {
+    let deck: Card[] = [];
+    this.cardKinds.forEach((kind, kindIndex) =>
+      this.faceValues.forEach((faceValue, faceValueIndex) => {
+        const cardId = (kindIndex + 1) * (faceValueIndex + 1);
+        deck.push(new Card(cardId, kind, faceValue));
+      })
+    );
+
+    return deck;
+  }
+
+  /**
+  * Shuffle an array following the Fisher-Yates shuffle (https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+  * @param deck the array of cards to shuffle
+  */
+  public shuffleDeck(deck: Card[]) {
+    for (let i = 0; i < deck.length; i++) {
+      const switchIndex = Math.floor(Math.random() * (i + 1));
+      const valueToSwitch = deck[i];
+      deck[i] = deck[switchIndex];
+      deck[switchIndex] = valueToSwitch;
+    }
+  }
+}
