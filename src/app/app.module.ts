5e9f851b393d8ddb54b655e5b4bfe64424808aed
Gabriel Beaulieu <gabrielbeaulieu117@gmail.com>
2018-11-17 20:19:04 -0500

    Remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoardModule } from './board/board.module';
 
 @NgModule({
   declarations: [
