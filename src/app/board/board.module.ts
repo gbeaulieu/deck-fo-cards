7b1fd0cc08af51db3e92bf69b7337a4267b95d9d
Gabriel Beaulieu <gabrielbeaulieu117@gmail.com>
2018-11-18 14:36:42 -0500

    Use mat-raised-button in BoardComponent

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { MatButtonModule } from '@angular/material';
 
 import { BoardComponent } from './board.component';
 import { BoardRoutingModule } from './board-routing.module';
@@ -9,7 +10,6 @@ import { CardListComponent } from './components/card-list/card-list.component';
 import { CardComponent } from './components/card/card.component';
 import { boardReducer } from './store/board.reducer';
 import { BoardEffects } from './store/board.effects';
-
 
 @NgModule({
   declarations: [
@@ -21,7 +21,8 @@ import { BoardEffects } from './store/board.effects';
     CommonModule,
     BoardRoutingModule,
     StoreModule.forFeature('boardState', boardReducer),
-    EffectsModule.forFeature([BoardEffects])
+    EffectsModule.forFeature([BoardEffects]),
+    MatButtonModule
   ]
 })
 export class BoardModule { }
